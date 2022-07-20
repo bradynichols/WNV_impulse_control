8bd38eb51bf78d7026a24b3d17997b9d69409cf4
SallyV7 <87834515+SallyV7@users.noreply.github.com>
2022-07-20 08:57:00 -0500

    Preliminary Changes 1 Adding Equations

--- System_parametersRL.m
+++ System_parametersRL.m
@@ -27,7 +27,7 @@ p(6) = 1/2; % hatch rate
 p(7) = 1/7; % larval maturation rate (1/larval lifespan) cite MosquitoLifecycleFINAL.
             % Here we combine the larval and pupal stages. 
 
-p(8) = 0.16; % Daily death rate of 1-3%.                   
+p(8) = 0.16; % Daily death (of larvae) rate of 1 - 3%.                   
 
              % Larval stage survival varied between 70-90%. 
              % This would mean p(7)/(p(7)+p(8))=.7-.9.
@@ -55,7 +55,7 @@ p(11)=.01; % mosquito larval carrying capacity.
 p(12) = 1/10; % disease progression in mosquitoes (1/latency period)
               % Cite West Nile Virus Infection Decreases Fecundity of Culex tarsalis Females
 
-p(13) = 0.5;  %mosquito-to-host transmission
+p(13) = 0.5; % mosquito-to-host transmission
 
 % HOST PARAMETERS
 % Host group 1: Birds with zero mortality from WNV.
@@ -201,13 +201,13 @@ mid_ef_day=34;
 mid_ef=.57;
 
 max_ef=1/(1+((min_ef/(1-min_ef))^(mid_ef_day/(min_ef_day-mid_ef_day)))*((1-mid_ef)/mid_ef)^(min_ef_day/(min_ef_day-mid_ef_day)));
-p(38) = max_ef*(p(8)+p(7))/(1-max_ef);
-p(39)= -log(mid_ef*(1-max_ef)/((1-mid_ef)*max_ef))/mid_ef_day;
+p(38) = max_ef*(p(8)+p(7))/(1-max_ef); % max rate at which larvicide kills larvae
+p(39)= -log(mid_ef*(1-max_ef)/((1-mid_ef)*max_ef))/mid_ef_day; % rate at which larvicide decays
 
 end
 
-p(40)=24; % adulticide decay rate
-p(41)=-log(0.1)*p(18)*2; % max rate at which adulticide kills adult vectors
+p(40)=-log(0.1)*p(18)*2; % max rate at which adulticide kills adult vectors
+p(41)=24; % adulticide decay rate
 per_remain_one_hour = exp(p(20)*0.5*exp(-p(18)/24)/p(18)-p(20)*0.5/p(18)); % Percent of adulticide remaining after one hour
 
 % Adulticide max kill rate estimated from mortality of caged mosquitoes in a fallow agricultural field.
@@ -233,9 +233,8 @@ p(43) = 1; %weight of cost of larvacide
 p(44) = 10; %weight of cost of adulticide
             %adulticide treatment is more expensive. Cite Mosquitoes and disease Illinois dept of public health
 p(45) = 0.05; % weight of cost of time
-p(46)=5000; %cost of eggs at the final time
-p(47)=-100000; %cost of hosts at the final time; value used in paper simulations
-
+p(46) = 5000; %cost of eggs at the final time
+p(47) = -100000; %cost of hosts at the final time; value used in paper simulations
 
 p(48)=Tf; % maximum time between controls
 p(49)=1; % minimum time between controls
--- West_Nile_ModelRL.m
+++ West_Nile_ModelRL.m
@@ -16,102 +16,140 @@ x1=x(1:13);
 x2=x(length(x1)+1:length(x1)+length(x1)^2);
 x2=reshape(x2,length(x1),length(x1));
 
-%Model Parameters
+% Model Parameters
 
-%Vector
-rs = p(1);            %intrinsic rate of increase of uninfected mosquitoes
-ri = p(2);           %intrinsic rate of increase of infected mosquitoes
-phi=p(3);               %fraction of eggs born to infected mothers that are infected
-qs=p(4);                %fraction of eggs from uninfected mosquitoes that hatch
-qi=p(5);                %fraction of eggs laid to infected mosquitoes that hatch
-m_e = p(6);              %egg maturatation rate
-m_l = p(7);             %larval maturation rate
+% Vector
+rs = p(1); % egg laying rate of S and E mosquitoes
+ri = p(2); % egg laying rate of I mosquitoes
 
-muL = p(8);           %larval death rate
-muV=p(9);             %adult death rate
-b = p(10);             %mosquito biting rate
-c_l = p(11);             %mosquito carrying capacity
+phi = p(3); % fraction of eggs born to infected mothers that are infected (fraction of eggs infected)
+qs = p(4); % fraction of eggs from uninfected mosquitoes that hatch
+qi = p(5); % fraction of eggs laid to infected mosquitoes that hatch
+
+m_e = p(6); % hatch rate 
+m_l = p(7); % larval maturation rate
+
+muL = p(8); % larval death rate
+muV = p(9); % adult death rate
+
+b = p(10); % mosquito biting rate
+c_l = p(11); % mosquito carrying capacity (larval)
 
 %Disease
-kl = p(12);           %disease progression (1/latency period)
 
-p_hm = p(13);     %host-to-mosquito transmission
-p_mh = p(14);     %mosquito-to-host transmission
-omega = p(32);     % Host-to-host transmission rate
+kl = p(12); % disease progression in mosquitoes (1/latency period)
+p_mh = p(13); % mosquito-to-host transmission
 
-dh = p(15);           %induced host mortality
-g = p(16);            %host recovery rate
+p_hm1 = p(14); % host-to-mosquito transmission host group 1
+p_hm2 = p(15); % host-to-mosquito transmission host group 2
+p_hm3 = p(16); % host-to-mosquito transmission host group 3
 
-gl=p(17);
-ga=p(18);
+omega1 = p(17); % direct transmission rate host group 1
+omega2 = p(18); % direct transmission rate host group 2
+omega3 = p(19); % direct transmission rate host group 3
 
-km1=p(19);
-km2=p(20);
+p_hh1 = p(20); % contact rate host group 1
+p_hh2 = p(21); % contact rate host group 2
+p_hh3 = p(22); % contact rate host group 3 
 
+g1 = p(23); % WNV recovery host group 1
+g2 = p(24); % WNV recovery host group 2
+g3 = p(25); % WNV recovery host group 3
 
-cV=p(21);                   %weight of cost of vectors in objective functional
+gamma1 = p(26); % WNV death host group 1
+gamma2 = p(27); % WNV death host group 2
+gamma3 = p(28); % WNV death host group 3
 
-Lambda = p(30); % Host recruitment rate
-gamma = p(31); % Host natural death rate
+Lambda1 = p(29); % recruitment rate host group 1
+Lambda2 = p(30); % recruitment rate host group 2
+Lambda3 = p(31); % recruitment rate host group 3
 
-omega = p(32); % Host-Host contact rate
-p_hh = p(33); %Host-Host transmission probability
+mu_h1 = p(32); % natural death rate host group 1
+mu_h2 = p(33); % natural death rate host group 2
+mu_h3 = p(34); % natural death rate host group 3
 
-c_h = p(34); % Host carrying capacity
+c_h1 = p(35); % carrying capacity host group 1
+c_h2 = p(36); % carrying capacity host group 2
+c_h3 = p(37); % carrying capacity host group 3
+
+km1 = p(38); % max rate at which larvicide kills larvae
+km2 = p(40); % max rate at which adulticide kills adult vectors
+
+gl = p(39); % larvicide decay rate
+ga = p(41); % adulticide decay rate
+
+cV = p(42) = 5000; % weight of cost of vectors in objective functional
 
 d_l=((rs*m_l*qs/muV)-muL-m_l)/c_l; % density-dependent death rate for larvae
-d_h = (Lambda - gamma)/c_h; % density-dependent death rate for host
+d_h1 = (Lambda1 - mu_h1)/c_h1; % density-dependent death rate for host group 1
+d_h2 = (Lambda2 - mu_h2)/c_h2; % density-dependent death rate for host group 2
+d_h3 = (Lambda3 - mu_h3)/c_h3; % density-dependent death rate for host group 3
 
-    %State variables
+% State variables
 
-    %Vector
-    Es = x(1);              %eggs laid by susceptible and exposed mothers
-    Ei = x(2);              %eggs laid by infected mothers
-    Ls = x(3);              %susceptible larvae
-    Li = x(4);              %infected larvae
-    Vs = x(5);              %susceptible vectors
-    Ve = x(6);              %exposed vectors
-    Vi = x(7);              %infected vectors
+% Vector
 
-    %Host
-    Hs = x(8);              %susceptible hosts
-    Hi = x(9);              %infected hosts
-    Hr = x(10);              %recovered hosts
+Es = x(1); % eggs laid by susceptible and exposed mothers
+Ei = x(2); % eggs laid by infected mothers
+Ls = x(3); % susceptible larvae
+Li = x(4); % infected larvae
+Vs = x(5); % susceptible vectors
+Ve = x(6); % exposed vectors
+Vi = x(7); % infected vectors
+
+% Host
+
+Hs1 = x(8); % susceptible host group 1
+Hs2 = x(9); % susceptible host group 2
+Hs3 = x(10); % susceptible host group 3
+
+Hi1 = x(11); % infected host group 1
+Hi2 = x(12); % infected host group 2
+Hi3 = x(13); % infected host group 3
+
+Hr1 = x(14); % recovered host group 1
+Hr2 = x(15); % recovered host group 2
+Hr3 = x(16); % recovered host group 3
     
-    %Control 
-    Ul = x(11);              %larvacide
-    Ua = x(12);             %adultacide
+% Control 
 
-    NH = Hs+Hi+Hr;           %total hosts
+Ul = x(11); % larvacide
+Ua = x(12); % adultacide
 
-    %Vector ODEs
-    dEs=  rs*(Vs+Ve)-m_e*Es;
-    dEi=  ri*(Vi)-m_e*Ei;
-    dLs = m_e*qs*Es+m_e*qi*(1-phi)*Ei-muL*Ls-m_l*Ls-d_l*Ls*(Ls+Li)-km1*Ls*Ul;
-    dLi = m_e*qi*phi*Ei-muL*Li-m_l*Li-d_l*Li*(Li+Ls)-km1*Li*Ul;
-    dVs = m_l*Ls-b*p_hm*Vs*Hi/NH-muV*Vs-km2*Vs*Ua;
-    dVe = b*p_hm*Vs*Hi/NH-kl*Ve-muV*Ve-km2*Ve*Ua;
-    dVi = m_l*Li+kl*Ve-muV*Vi-km2*Vi*Ua;
+NH = Hs+Hi+Hr; % total hosts
 
-    %Host ODEs
-    dHs = Lambda*Hs - b*p_mh*Vi*Hs/NH - omega*p_hh*Hi*Hs/NH - d_h*NH*Hs - gamma*Hs; % Updated 07/19/2022
-%                        [V_i]*[prob of transmission]*[bites per unit time per mosquito]*[number of suceptibles]/[number of hosts]=
-%                           =transmission events per unit time.
-%                       Note the rate at which a host becomes infected increases with bite rate.
-    dHi = b*p_mh*Vi*Hs/NH + omega*p_hh*Hi*Hs/NH - (dh+g)*Hi - d_h*NH*Hi - gamma*Hi; % Updated 07/19/2022
-    dHr = g*Hi - d_h*NH*Hr - gamma*Hr; % Updated 07/19/2022
+% Vector ODEs
+
+dEs = rs*(Vs+Ve)-m_e*Es;
+dEi = ri*(Vi)-m_e*Ei;
+dLs = m_e*qs*Es+m_e*qi*(1-phi)*Ei-muL*Ls-m_l*Ls-d_l*Ls*(Ls+Li)-km1*Ls*Ul;
+dLi = m_e*qi*phi*Ei-muL*Li-m_l*Li-d_l*Li*(Li+Ls)-km1*Li*Ul;
+dVs = m_l*Ls-b*p_hm*Vs*Hi/NH-muV*Vs-km2*Vs*Ua;
+dVe = b*p_hm*Vs*Hi/NH-kl*Ve-muV*Ve-km2*Ve*Ua;
+dVi = m_l*Li+kl*Ve-muV*Vi-km2*Vi*Ua;
+
+% Host ODEs
+
+dHs1 = Lambda*Hs - b*p_mh*Vi*Hs/NH - omega*p_hh*Hi*Hs/NH - d_h*NH*Hs - gamma*Hs; % Updated 07/19/2022
+dHs2 = 
+dHs3 = 
+dHi1 = b*p_mh*Vi*Hs/NH + omega*p_hh*Hi*Hs/NH - (dh+g)*Hi - d_h*NH*Hi - gamma*Hi; % Updated 07/19/2022
+dHi2 = 
+dHi3 = 
+dHr1 = g*Hi - d_h*NH*Hr - gamma*Hr; % Updated 07/19/2022
+dHr2 = 
+dHr3 = 
     
-    %Chemical ODEs
-    dUl=-gl*Ul;
-    dUa=-ga*Ua;
-    
-    %Integral State ODE
-    dx_int=cV*(Vi+Vs+Ve); 
+%Chemical ODEs
 
+dUl = -gl*Ul;
+dUa = -ga*Ua;
+    
+% Integral State ODE
+
+dx_int = cV*(Vi+Vs+Ve); 
 
 f=[dEs; dEi; dLs; dLi; dVs; dVe; dVi; dHs; dHi; dHr; dUl; dUa; dx_int];
-
-% Updated to include host demographics
 
 dfdx=[ -m_e, 0, 0, 0, rs, rs, 0, 0, 0, 0, 0, 0, 0;
         0, -m_e, 0, 0, 0, 0, ri, 0, 0, 0, 0, 0, 0;
