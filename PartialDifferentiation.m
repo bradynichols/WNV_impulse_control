cbb8b5e1c955964f34adc0903284e3e35c8f8a5d
Brady Nichols <bnichols@bowdoin.edu>
2022-07-20 12:30:31 -0500

    Added Jacobian.m to calculate the jacobians in the model files

--- PartialDifferentiation.m
+++ PartialDifferentiation.m
@@ -1,7 +1,7 @@
-syms Lambda1 Hs1 b p_mh Vi Hi1 Hr1 omega1 p_hh1 d_h1 mu_h1
+syms Lambda1 Hs1 b p_mh Vi Hi1 Hr1 omega1 p_hh1 d_h1 mu_h1 Ls p_hm1 Vs NH1 p_hm2 Hi2 NH2 p_hm3 Hi3 NH3 Ua
 
 % (Hs1 + Hi1 + Hr1)
 
-y = Lambda1*Hs1 - b*p_mh*Vi*Hs1/(Hs1 + Hi1 + Hr1) - omega1*p_hh1*Hi1*Hs1/(Hs1 + Hi1 + Hr1) - d_h1*(Hs1 + Hi1 + Hr1)*Hs1 - mu_h1*Hs1;
+y = m_l*Ls - b*p_hm1*Vs*Hi1/NH1 - b*p_hm2*Vs*Hi2/NH2 - b*p_hm3*Vs*Hi3/NH3 - muV*Vs - km2*Vs*Ua;
 
-diff(y,Hr1)
+diff(y,Vs)
